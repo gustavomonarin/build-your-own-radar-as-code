1415b6f173fd0fd2d7ef65082a2a6dd9e195cfb4
gustavomonarin <gustavomonarin@gmail.com>
2020-10-23 14:48:54 +0200

    feat: introducer input and output command line args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use structopt::StructOpt;
 use serde::{Serialize, Deserialize};
 use std::{fs, io, process};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::error::Error;
+use std::borrow::Borrow;
 
 #[derive(Deserialize, Serialize, Debug)]
-#[serde(rename_all="camelCase")]
+#[serde(rename_all = "camelCase")]
 struct Blip {
     name: String,
     quadrant: String,
@@ -13,8 +15,7 @@ struct Blip {
     description: String,
 }
 
-fn convert_from_blips_yaml_files_to_csv_file(input_folder: &Path, output_file: &Path)  -> Result<(), Box<dyn Error>>  {
-
+fn convert_from_blips_yaml_files_to_csv_file(input_folder: &Path, output_file: &Path) -> Result<(), Box<dyn Error>> {
     let mut writer = csv::Writer::from_path(output_file).unwrap();
 
     let paths = fs::read_dir(input_folder).unwrap();
@@ -49,24 +50,43 @@ fn is_valid_blip_file(entry: &Result<fs::DirEntry, io::Error>) -> bool {
     )
 }
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "blipnize",
+about = "A command to convert simple yaml blip files to the format expected by the thoughtworks radar")]
+struct CommandArgs {
+    /// The input dir containing the multiple blip yaml files. The files must have the extension .yaml
+    #[structopt(short = "d", long = "--input-dir")]
+    input_dir: PathBuf,
+
+    /// The output file containing the aggregated blips in the expected csv format
+    #[structopt(short = "o", long = "--output-file")]
+    output_file: PathBuf,
+}
 
 fn main() {
+    let args : CommandArgs = CommandArgs::from_args();
+    validate_input_dir(args.input_dir.borrow());
+    validate_output_file(args.output_file.borrow());
 
-    let path = Path::new("./blips-thoughtworks-vol21/");
-
-    let output = Path::new("/tmp/output.csv");
-    validate_output_file(output);
-
-    if let Err(err) = convert_from_blips_yaml_files_to_csv_file(path, output) {
+    if let Err(err) = convert_from_blips_yaml_files_to_csv_file(
+        args.input_dir.borrow(),
+        args.output_file.borrow()
+    ) {
         println!("{}", err);
         process::exit(1);
     }
+}
 
+fn validate_input_dir(input_dir: &Path){
+    if !input_dir.exists() {
+        eprintln!("error: Input dir does not exists '{}' .", input_dir.display());
+        process::exit(1); // should avoid multiple exits. TODO: convert everything to a Result and chain them
+    }
 }
 
 fn validate_output_file(output_file: &Path) {
     if output_file.exists() {
-        eprintln!("ERROR: Output file '{}' already exists.", output_file.display());
+        eprintln!("error: Output file '{}' already exists.", output_file.display());
         process::exit(1); // should avoid multiple exits. TODO: convert everything to a Result and chain them
     }
 }
