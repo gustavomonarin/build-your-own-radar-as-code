6e52b782cb0cd5c6fe52d6649f6a45d90fcd2ee0
gustavomonarin <gustavomonarin@gmail.com>
2020-10-23 11:21:56 +0200

    fail if output file already exists

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use std::{fs, io};
+use std::path::Path;
 
 #[derive(Deserialize, Serialize, Debug, )]
 #[serde(rename_all="camelCase")]
@@ -13,7 +14,10 @@ struct Blip {
 
 fn main() {
 
-    let mut writer = csv::Writer::from_path("/tmp/output.csv").unwrap();
+    let output = Path::new("/tmp/output.csv");
+    validate_output_file(output);
+
+    let mut writer = csv::Writer::from_path(output).unwrap();
 
     let paths = fs::read_dir("./blips-thoughtworks-vol21/").unwrap();
     paths
@@ -43,4 +47,12 @@ fn is_valid_blip_file(entry: &Result<fs::DirEntry, io::Error>) -> bool {
         false,
         |e| e.file_name().to_str().unwrap().ends_with(".yaml"),
     )
+}
+
+
+fn validate_output_file(output_file: &Path) {
+    if output_file.exists() {
+        eprintln!("ERROR: Output file '{}' already exists.", output_file.display());
+        std::process::exit(1); // should avoid multiple exits. TODO: convert everything to a Result and chain them
+    }
 }
